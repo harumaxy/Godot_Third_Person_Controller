1656e456f25b51b05eb397a2e72de0e0cecc4853
max <harumaxy@gmail.com>
2021-07-21 18:02:25 +0900

    add player camera & mesh rotation controle

--- src/Player.gd
+++ src/Player.gd
@@ -9,7 +9,8 @@ const gravity = Globals.gravity
 var direction := Vector3.FORWARD
 var velocity = Vector3.ZERO
 var vertical_vel = 0
-var acceleration = 6
+var acceleration = 5
+var angular_acceleration = 7
 
 # ColorRect + Label で、キー入力を可視化するUIを作る
 func _input(event):
@@ -28,13 +29,23 @@ func _get_direction_input(h_rot: float):
   var z := Input.get_action_strength("forward") - Input.get_action_strength("backward")
   return Vector3(x, 0, z).rotated(Vector3.UP, h_rot).normalized()
 
+
 func _physics_process(delta):
   var h_rot = $CameraRoot/h.global_transform.basis.get_euler().y
   var direction = _get_direction_input(h_rot)
   var speed = run_speed if Input.is_action_pressed("sprint") else walk_speed
-  self.move_and_slide(direction * speed * delta, Vector3.UP)
+  velocity = lerp(velocity, direction * speed, delta * acceleration)
+  self.move_and_slide(velocity * delta + vertical_vel * Vector3.DOWN , Vector3.UP)
 
+  # 垂直の速度と水平の速度は別で管理
   if !self.is_on_floor():
     vertical_vel += gravity * delta
   else:
     vertical_vel = 0
+
+  # カメラの回転と、Mesh の回転は別管理
+  # lerp_angle : 角度を線形補間
+  # atan2() : Vector2(y, x)  を与えその角度を返す
+  if direction != Vector3.ZERO:
+    $Mesh.rotation.y = lerp_angle($Mesh.rotation.y, atan2(direction.x, direction.z), angular_acceleration * delta)
+
